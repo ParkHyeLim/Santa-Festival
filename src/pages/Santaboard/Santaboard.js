38d28f13463cf3cbbe5da8816dd86bc851dd9850
ParkHyeLim <ajtwoddl042504@dongguk.edu>
2025-12-22 19:36:47 +0900

    feat : 당첨자 페이지 이미지 추가

--- src/pages/Santaboard/Santaboard.js
+++ src/pages/Santaboard/Santaboard.js
@@ -4,7 +4,7 @@ import page1 from "../../assets/images/Santaboard1.jpg";
 function Santaboard() {
   return (
       <div className="SantaContainer">
-        {/* <img className="imageStyle" src={page1} alt="test1" /> */}
+        <img className="imageStyle" src={page1} alt="test1" />
       </div>
   );
 }
